1624cb573d9d6e51feef85bd5c36c05d63dc5f70
Nikita Anishenco <tanishenco91@gmail.com>
2023-10-03 10:35:45 +0300

    В 1 добавлен класс Круга берущий метод из Фигуры и вычисляет площади круга

--- Module_3/1_BaseFigure.cs
+++ Module_3/1_BaseFigure.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Module_3
 {
-    internal class _1_BaseFigure
+    public delegate double AreaDelegate();
+    public abstract class Figure
     {
+        public abstract double CalculatingArea();
+    }
+
+    public class Circle : Figure
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculatingArea()
+        {
+            return Math.PI * Math.Pow(radius, 2); // формула вычисления площади круга
+        }
+    }
+
+    public abstract class Rectangle : Figure
+    {
+
     }
 }
