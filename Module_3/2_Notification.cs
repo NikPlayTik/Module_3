233b8c8e1b93448926982c4af3374b072c699ad9
Nikita Anishenco <tanishenco91@gmail.com>
2023-10-03 18:58:35 +0300

    В 2 добавлена основная логика программы по событиям, также добавлено меню для выбора типа уведомления

--- Module_3/2_Notification.cs
+++ Module_3/2_Notification.cs
@@ -6,7 +6,73 @@ using System.Threading.Tasks;
 
 namespace Module_3
 {
-    internal class _2_Notification
+    // Класс "Уведомление"
+    public class Notification
     {
+        public event Action<string> OnMessage;
+        public event Action<string> OnCall;
+        public event Action<string> OnEmail;
+        public bool flagBool;
+
+        public Notification() 
+        {
+            flagBool = true;
+        }
+        /*Оператор условного null ?. проверяет, не равно ли выражение слева
+        от него null, прежде чем пытаться вызвать метод или свойство справа от него.*/
+        public void SendMessage(string message)
+        {
+            OnMessage?.Invoke(message);
+        }
+
+        public void MakeCall(string number)
+        {
+            OnCall?.Invoke(number);
+        }
+
+        public void SendEmail(string email)
+        {
+            OnEmail?.Invoke(email);
+        }
+
+        public void OutputData()
+        {
+            Notification notification = new Notification();
+            // message и т.д - анонимный метод
+            notification.OnMessage += message => Console.WriteLine($"Отправка сообщения: {message}");
+            notification.OnCall += number => Console.WriteLine($"Вызов на номер: {number}");
+            notification.OnEmail += email => Console.WriteLine($"Отправка сообщения на электронную почту: {email}");
+
+            while (flagBool)
+            {
+                Console.Write("Выберите тип уведомления (1 - сообщение, 2 - звонок, 3 - электронное письмо, 4 - выход): ");
+                string choiceNumber = Console.ReadLine();
+                switch (choiceNumber)
+                {
+                    case "1":
+                        Console.Write("Введите сообщение: ");
+                        string message = Console.ReadLine();
+                        notification.SendMessage(message);
+                        break;
+                    case "2":
+                        Console.Write("Введите номер: ");
+                        string number = Console.ReadLine();
+                        notification.MakeCall(number);
+                        break;
+                    case "3":
+                        Console.Write("Введите адрес электронной почты: ");
+                        string email = Console.ReadLine();
+                        notification.SendEmail(email);
+                        break;
+                    case "4":
+                        flagBool = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неверный выбор");
+                        break;
+                }
+            }
+                
+        }
     }
 }
