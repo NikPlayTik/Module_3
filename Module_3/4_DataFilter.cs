a9b5fb628e35e4f6e3442b6baf83f407ba03a48e
Nikita Anishenco <tanishenco91@gmail.com>
2023-10-04 12:48:31 +0300

    В 4 заново переработана вся логика и фильтрация данных, добавлена проверка ещё одна на ввод даты и добавление в список

--- Module_3/4_DataFilter.cs
+++ Module_3/4_DataFilter.cs
@@ -11,51 +11,11 @@ namespace Module_3
 
     class Filtered
     {
-        public string inputText;
-        public void OutputData()
+        private string inputText;
+
+        public Filtered()
         {
-            Console.Write("Сколько дат вы хотите ввести: ");
-            int dataCount = int.Parse(Console.ReadLine());
-
-            List<string> dataList = new List<string>();
-
-            for (int i = 0; i < dataCount; i++)
-            {                
-                do
-                {
-                    Console.Write("Введите дату (дд.мм.гггг): ");
-                    inputText = Console.ReadLine();
-                } while (!Regex.IsMatch(inputText, @"^\d{2}\.\d{2}\.\d{4}$"));
-
-                dataList.Add(inputText);
-            }    
-
-                while (true)
-            {
-                Console.WriteLine("Выберите фильтр (1 - По дате, 2 - По ключевым словам, 3 - Выход): ");
-                int choice = int.Parse(Console.ReadLine());
-
-                if (choice == 1)
-                {
-                    Console.Write("Введите дату для фильтрации: ");
-                    string filter = Console.ReadLine();
-                    FilterData(dataList, FilterByDate, filter);
-                }
-                else if (choice == 2)
-                {
-                    Console.Write("Введите ключевое слово для фильтрации: ");
-                    string filter = Console.ReadLine();
-                    FilterData(dataList, FilterByKeyword, filter);
-                }
-                else if (choice == 3)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Некорректный выбор. Попробуйте еще раз.");
-                }
-            }
+            inputText = "Нет даты";
         }
 
         // метод для фильтрации данных по дате
@@ -63,13 +23,11 @@ namespace Module_3
         {
             return dataItem.Contains(filter);
         }
-
         // метод для фильтрации данных по ключевым словам
         private bool FilterByKeyword(string dataItem, string filter)
         {
             return dataItem.Contains(filter);
         }
-
         // метод для применения фильтра к данным с использованием делегата
         private void FilterData(List<string> dataList, DataFilterDelegate filterDelegate, string filter)
         {
@@ -81,6 +39,67 @@ namespace Module_3
                 Console.WriteLine(item);
             }
         }
+        public void OutputData()
+        {
+            Console.Write("Сколько дат вы хотите ввести: ");
+            int dataCount = int.Parse(Console.ReadLine());
+            List<string> dataList = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("Выберите действие (1 - Ввести дату, 2 - Фильтровать данные, 3 - Выход): ");
+                int choiceNumber = int.Parse(Console.ReadLine());
+                switch (choiceNumber)
+                {
+                    case 1:
+                        for(int i = 0; i < dataCount; i++)
+                        { 
+                            Console.Write("Введите дату (дд.мм.гггг): ");
+                            inputText = Console.ReadLine();
+                            if (!Regex.IsMatch(inputText, @"^\d{2}\.\d{2}\.\d{4}$"))
+                            {
+                                Console.WriteLine("Некорректный формат даты");
+                            }
+                        }
+                        dataList.Add(inputText);
+                        break;
+                    case 2:
+                        Console.WriteLine("Выберите фильтр (1 - По дате, 2 - По ключевым словам): ");
+                        int filterChoice = int.Parse(Console.ReadLine());
+                        switch (filterChoice)
+                        {
+                            case 1:
+                                Console.Write("Введите дату для фильтрации: ");
+                                string dateFilter = Console.ReadLine();
+                                if (Regex.IsMatch(inputText, @"^\d{2}\.\d{2}\.\d{4}$"))
+                                {
+                                    FilterData(dataList, FilterByDate, dateFilter);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректный ввод");
+                                }
+                                break;
+                            case 2:
+                                Console.Write("Введите ключевое слово для фильтрации: ");
+                                string keywordFilter = Console.ReadLine();
+                                FilterData(dataList, FilterByKeyword, keywordFilter);
+                                break;
+                            default:
+                                Console.WriteLine("Некорректный выбор");
+                                break;
+                        }
+                        break;
+                    case 3:
+                        return;
+                    default:
+                        Console.WriteLine("Некорректный выбор. Попробуйте еще раз.");
+                        break;
+                }
+            }
+        }
+
+        
     }
 
 }
